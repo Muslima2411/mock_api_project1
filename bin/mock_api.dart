ac3dd442d5dc8b2e839b0060fd084b3672d068cb
muslima <muslima241105@gmail.com>
2023-11-24 10:30:34 +0500

    My first commit

---
+++ bin/mock_api.dart
@@ -0,0 +1,5 @@
+import 'package:mock_api/mock_api.dart' as mock_api;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mock_api.calculate()}!');
+}
---
+++ test/mock_api_test.dart
@@ -0,0 +1,8 @@
+import 'package:mock_api/mock_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
